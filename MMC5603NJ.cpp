0e91cac9d7de3e41739a830d7219c568fea64d90
Nick <nick.huttemann@gmail.com>
2020-05-05 12:52:19 -0700

    final version

--- MMC5603NJ.cpp
+++ MMC5603NJ.cpp
@@ -1,44 +1,125 @@
 #include "MMC5603NJ.h"
 
-MMC5603NJ::MMC5603NJ(I2C ext_i2c, int odr) {
+
+MMC5603NJ::MMC5603NJ(I2C* ext_i2c, int odr, int BW) {
     i2c = ext_i2c;
     _odr = odr;
     setODR(_odr);
+    setBW(BW);
 }
 
-MMC5603NJ::setODR(int odr) {
+bool MMC5603NJ::setODR(int odr) {
+    char data[2];
+    data[0] = ODR;
+    data[1] = (char) odr;
     
     if (odr >=0 && odr <= 255) {
-        char[] data = {ODR, (char) odr};
-
-        return(!i2c.write(MMC5603NJ_WRITE, data, 2))
+        return(!i2c->write(MMC5603NJ_WRITE, data, 2));
     }
     return false;
 }
 
-MMC5603NJ::startContinuousMode() {
-    
-    const char[] data1 = {CTRL_0, CTRL_0_CONTINUOUS};
-    const char[] data2 = {CTRL_2, CTRL_2_CONTINUOUS_START};
-    
-    if (!i2c.write(MMC5603NJ_WRITE, data1, 2)) {
-        
-        if(!i2c.write(MMC5603NJ_WRITE, data2, 2 )) {
+bool MMC5603NJ::setBW(int BW) {
+    char data[2];
+    data[0] = CTRL_1;
+    data[1] = (char) BW;
 
+    if (BW == CTRL_1_BW_75HZ || BW == CTRL_1_BW_150HZ || BW == CTRL_1_BW_255HZ || BW == CTRL_1_BW_1000HZ) {
+        return(!i2c->write(CTRL_1, data, 2));
+    }
+    return false;
+}
+
+bool MMC5603NJ::startContinuousMode() {
+    char data1[2];
+    data1[0] = CTRL_0;
+    data1[1] = CTRL_0_CONTINUOUS;
+
+    char data2[2];
+    data2[0] = CTRL_2;
+    data2[1] = CTRL_2_CONTINUOUS_START;
+    //data2[1] = CTRL_2_CONTINUOUS_START_HIPWR; uncomment for high power mode
+    
+    if (!i2c->write(MMC5603NJ_WRITE, data1, 2)) {
+        
+        if(!i2c->write(MMC5603NJ_WRITE, data2, 2 )) {
             return true;
         }
     }
     return false;
 }
 
-MMC5603NJ::getProductID() {
+bool MMC5603NJ::setHiPWR() {
+    char data[2];
+    data[0] = CTRL_2;
+    data[1] = CTRL_2_HIPWR;
 
-    const char data* = PRODUCT_ID;
+    if (!i2c->write(MMC5603NJ_WRITE, data, 2)) {
+        return true;
+    }
+    return false;
+}
+
+int MMC5603NJ::getProductID() {
+
+    const char data = PRODUCT_ID;
     char dataOut = 0x00;
 
-    if (!i2c.write(MMC5603NJ_WRITE, data, 1)) {
-        return (int) i2c.read(MMC5603NJ_READ, &dataOut, 1);
+    if (!i2c->write(MMC5603NJ_WRITE, &data, 1)) {
+        i2c->read(MMC5603NJ_READ, &dataOut, 1);
+        return (int) dataOut;
     };
+    return -1;
+}
+
+bool MMC5603NJ::takeMeasurement() {
+    char data[2];
+    data[0] = CTRL_0;
+    data[1] = CTRL_0_TAKE_MEASUREMENT;
+
+    if (!i2c->write(MMC5603NJ_WRITE, data, 2)) {
+        return true;
+    }
+    return false;
+}
+
+float32_t MMC5603NJ::getMeasurement(bool wait) {
+
+    char status_reg = STATUS_1;
+    char status;
+    float32_t x, y, z;
+
+    char axisData[9];
+    char xout0 = X_OUT_0;
+    
+    // Wait until the measurement ready bit is set in the status register
+    if (wait) {
+        do {
+            if (!i2c->write(MMC5603NJ_WRITE, &status_reg, 1)) {
+                i2c->read(MMC5603NJ_READ, &status, 1);
+                //printf("STATUS: %d\n", status);
+            }
+        }   
+        while(!(status & 0x40));
+    }
+    
+
+    if (!i2c->write(MMC5603NJ_WRITE, &xout0, 1)) {
+        i2c->read(MMC5603NJ_READ, axisData, 9);
+    }
+
+    // printf("Xout0: %d, Xout1: %d, Xout2: %d\n"
+    //        "Yout0: %d, Yout1: %d, Yout2: %d\n"
+    //        "Zout0: %d, Zout1: %d, Zout2: %d\n", 
+    // axisData[0],axisData[1],axisData[6],axisData[2],axisData[3],axisData[7],axisData[4],axisData[5],axisData[8]);
+
+    //3 Bytes of data are returned for each axis so we have to combine them together
+    x = (float32_t) (((axisData[0] << 12) + (axisData[1] << 4) + (axisData[6] >> 4)) * sensitivity) - 32768;
+    y = (float32_t) (((axisData[2] << 12) + (axisData[3] << 4) + (axisData[7] >> 4)) * sensitivity) - 32768;
+    z = (float32_t) (((axisData[4] << 12) + (axisData[5] << 4) + (axisData[8] >> 4)) * sensitivity) - 32768;
+
+    return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+
 }
 
 
--- MMC5603NJ.h
+++ MMC5603NJ.h
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include "arm_math.h"
 
 // Register Addresses
 #define X_OUT_0 	0x00 
@@ -31,24 +32,45 @@
 // Register Configurations
 #define CTRL_0_CONTINUOUS_AUTO_SR 0xA0
 #define CTRL_0_CONTINUOUS 0x80
+#define CTRL_0_TAKE_MEASUREMENT 0x01
+
+#define CTRL_1_BW_75HZ 0x00
+#define CTRL_1_BW_150HZ 0x01
+#define CTRL_1_BW_255HZ 0x02
+#define CTRL_1_BW_1000HZ 0x03
 
 #define CTRL_2_CONTINUOUS_START 0x10
+#define CTRL_2_CONTINUOUS_START_HIPWR 0x90
+#define CTRL_2_HIPWR 0x80
+
+#define STATUS_1_MEAS_DONE 0x40
+
+// Misc
+#define T_WAIT_BW_75 6.6
+#define T_WAIT_BW_150 3.5
+#define T_WAIT_BW_255 2.0
+#define T_WAIT_BW_HPOWER 1.2
+
 
 
 class MMC5603NJ { 
 
 public:
 
-    MMC5603NJ (I2C ext_i2c, PinName sda, PinName, scl);
+    MMC5603NJ (I2C* ext_i2c, int odr, int BW);
 
     bool setODR(int odr);
+    bool setBW(int BW);
     bool startContinuousMode();
+    bool takeMeasurement();
+    bool setHiPWR();
     int getProductID();
-    float getMag();
+    float32_t getMeasurement(bool wait);
+
+    I2C* i2c;
 
 private:
 
-    I2C i2c;
     int _odr;
-
-}
+    float32_t sensitivity = 0.0625;
+};
